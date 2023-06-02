1b459f1b28a58408a64906366fa1c29ef77f20a3
Moayad Abukhadra <moayadabukhadra54@gmail.com>
2023-06-02 20:35:59 +0300

    Update Auth.js

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -12,8 +12,20 @@ const registerJobSeeker = (data) => {
     return api.post('/auth/job-seeker/register', data);
 }
 
+const forgotPassword = (data) => {
+    /* required data: email */
+    return api.post('/auth/forgot-password', data);
+}
+
+const resetPassword = (data) => {
+    /* required data: token, password, password_confirmation */
+    return api.post('/auth/reset-password', data);
+}
+
 export default {
     login,
     registerEmployer,
-    registerJobSeeker
+    registerJobSeeker,
+    forgotPassword,
+    resetPassword
 };
